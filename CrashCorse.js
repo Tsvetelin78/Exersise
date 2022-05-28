62b5c960cb9e5054cd8ac4b98d0f40b4e61d29dd
Tsvetelin Iliev <iamonfire23@abv.bg>
2022-05-28 16:00:20 +0300

    first commit

---
+++ CrashCorse.js
@@ -0,0 +1,172 @@
+//log a statement using console.log
+//console.log('Hello from CrashCorse.js')
+
+
+// let age = 25
+//console.log(age)
+
+//const salary = 8000
+//console.log(salary)
+
+//const sala = 580000
+//console.log(salary,'EBAGO U DOLARITE')
+
+// let sum = 0
+
+// sum =5
+// console.log(sum)
+
+// const name ='Vishwas'
+// const language = "JavaScript"
+// const channel = 'CodeVOLUTION'
+
+// const total = 0
+// const PI = 3.14
+
+// const isPrimaryNumber = true
+// const isNewUser = false
+
+
+
+// const res = null
+// const data = null
+
+// const person ={
+//     firstName: 'Bruce',
+//     lastName: 'Wayne',
+//     age: 25
+// }
+// console.log(person.firstName)
+
+// const oddNumbers = [1,3,5,7,9]
+// console.log(oddNumbers[3])
+
+// let a = 10
+// a = 'Vishwas'
+// a = true
+// a = 'Vishwas'
+// console.log(a)
+
+// let x = 10
+// let y = 5
+// console.log(x % y)
+// console.log(++x)
+// console.log(--y)
+// console.log(x <= y)
+
+// const isValidNumber = x > 11 || 3 < y
+// console.log(isValidNumber)
+
+// const isValid = false
+// console.log(!isValid)
+
+// console.log('Bruce '+ 'Wayne')
+
+// const isEven = 10 % 2 === 0 ? 'Number is even' : 'Numer is odd'
+// console.log(isEven)
+
+// console.log(true +'3')
+// console.log('4' - '3')
+// console.log('4' - true)
+// console.log('4' - false)
+// console.log('Bruce ' + 'Wayne')
+// console.log('5' - null)
+// console.log('Bruce ' + null)
+// console.log(5 + undefined)
+
+// console.log(Number(''))
+// console.log(parseInt('5'))
+// console.log(parseFloat('5'))
+// console.log(String(500))
+// console.log((500).toString())
+//console.log(Boolean(10)) // null undefined 0 '' Nan
+
+// const var1 = 10
+// const var2 = '10'
+// console.log(var1 == var2)
+// console.log(var1 === var2)
+
+
+// const num = 0
+// if(num > 0)
+// {
+//     console.log("Number is positive")
+// }
+// else if(num < 0){  
+//        console.log("Number is not positive")
+// }
+// else{console.log("Number is zero")}
+
+// const color = 'red'
+
+// switch(color){
+//     case 'red': console.log('Color is red')
+//         break
+//     case 'blue': console.log('Color is blue')
+//         break
+//     case 'green': console.log('Color is green')
+//         break
+//     default: console.log('not a valid color')
+
+// }
+
+// for (let i=1; i<=5; i++)
+// {
+//     //console.log('JavaScript')
+//     console.log('Iteration ' + i)
+// }
+
+// let num = 1
+// while (num <= 5)
+// {
+//     console.log('Iteration ' + num)
+//     num++
+
+// }
+// let i = 1
+// do {
+//     console.log('Iteration ' + i)
+//     i++
+// }while( i<=5)
+
+
+// const numArray =[1,2,3,4,5]
+// for (const num of numArray)
+// {
+//     console.log('iteration number ' + num)
+   
+// }
+// function great(username)
+// {
+//     console.log('Hello ' + username)
+// }
+// function add(a, b)
+// {
+//     return a + b
+// }
+// const arrowSum = (a, b) => a + b
+
+// const arrowFive = (num) => num + 5
+
+// const sume = arrowFive(10)
+// console.log(sume)
+// const myNum = 100
+
+// if(true){
+//     const myName = "Tseko"
+//     console.log(myName)
+//     console.log(myNum)
+// }
+
+// function testFn(){
+//     const q = "sDedo"
+//     console.log(q)
+//     console.log(myNum)
+// } 
+// testFn()
+
+const array1 = [1, 2, 3];
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+console.log(array1.reduce(reducer));
+
